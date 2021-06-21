d109ee37d87cd29a51249339e9bcda8c2d05d044
AnantSharma94 <81180752+AnantSharma94@users.noreply.github.com>
2021-06-21 15:12:39 +0530

    Quiz-Generator initial commit

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ const App = () => {
        const categoryData = await requestCategories.json();
        
        setCategories(categoryData.trivia_categories);
-       console.log(categoryData);
+       //console.log(categoryData);
      }
      loadCategories();
    }, [])
 
   useEffect(() => {
-    console.log(flashcards);
+   // console.log(flashcards);
   }, [flashcards])
 
   const decodeString = str => {
@@ -33,7 +33,7 @@ const App = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(amountEl.current.value+" "+categoryEl.current.value);
+    //console.log(amountEl.current.value+" "+categoryEl.current.value);
     const request = await fetch(`https://opentdb.com/api.php?amount=${amountEl.current.value}&categoty=${categoryEl.current.value}`);
       const data = await request.json();  
       
